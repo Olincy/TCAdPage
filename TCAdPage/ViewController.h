926a019c686482fb8c0dd8077022fae5c2882aa2
lincy <lincuiyang@talicai.com>
2016-05-06 18:08:27 +0800

    Initial Commit

---
+++ TCAdPage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TCAdPage
+//
+//  Created by lincy on 16/5/6.
+//  Copyright © 2016年 lincy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TCAdPageTests/TCAdPageTests.m
@@ -0,0 +1,39 @@
+//
+//  TCAdPageTests.m
+//  TCAdPageTests
+//
+//  Created by lincy on 16/5/6.
+//  Copyright © 2016年 lincy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TCAdPageTests : XCTestCase
+
+@end
+
+@implementation TCAdPageTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
